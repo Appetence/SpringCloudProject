1ddcaf9480bcfda41c39c1797e575753cb6f4cfc
15834260040 <120519>
2020-04-22 10:38:01 +0800

    测试网关

--- cloud-gateway-getway9527/src/main/java/com/ztx/springcloud/GetWay9527Application.java
+++ cloud-gateway-getway9527/src/main/java/com/ztx/springcloud/GetWay9527Application.java
@@ -3,10 +3,12 @@ package com.ztx.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringCloudApplication
+@RefreshScope//bush配置
 public class GetWay9527Application {
     public static void main(String[] args) {
         SpringApplication.run(GetWay9527Application.class,args);
